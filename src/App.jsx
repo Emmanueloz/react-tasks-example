93108ab2df3936dd422048c097864e807b139a9f
Emmanueloz <davidozuna84@hotmail.com>
2023-03-04 15:37:33 -0600

    Añadir tareas al formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
+import { useEffect, useState } from "react";
+import { tasks as data } from "./tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(taskTitle) {
+    setTasks([
+      ...tasks,
+      {
+        title: taskTitle,
+        id: tasks.length,
+      },
+    ]); //Valore que contiene el arreglo, valores nuevos que tendrá el arreglo
+  }
   return (
-    <div>
-      <TaskList />
-    </div>
+    <>
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks} />
+    </>
   );
 }
 
---
+++ src/TaskForm.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function TaskForm({ createTask }) {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask(title)
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Escribe tu tarea"
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button>Guardar</button>
+    </form>
+  );
+}
+
+export default TaskForm;
