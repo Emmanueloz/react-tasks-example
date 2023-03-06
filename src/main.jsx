9a87ad32932c0304db0d164186f38816a4410eea
Emmanueloz <davidozuna84@hotmail.com>
2023-03-05 18:26:17 -0600

    Crear contexto

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { TaskContextProvider } from "./context/TaskContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>{/* Código correcto de react */}
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* Código correcto de react */}
+    <TaskContextProvider>
+      <App />
+    </TaskContextProvider>
+  </React.StrictMode>
+);
