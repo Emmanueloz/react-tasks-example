496a6904519b89556ed129dd896ff3750b5ac368
Emmanueloz <davidozuna84@hotmail.com>
2023-03-05 18:47:50 -0600

    useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
-import { useEffect, useState } from "react";
-import { tasks as data } from "./data/tasks";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    setTasks(data);
-  }, []);
-
-  function createTask(task) {
-    setTasks([
-      ...tasks,
-      {
-        title: task.title,
-        id: tasks.length,
-        description: task.description,
-      },
-    ]); //Valore que contiene el arreglo, valores nuevos que tendrá el arreglo
-  }
-
-  function deleteTask(taskId) {
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  }
-
   return (
     <>
-      <TaskForm createTask={createTask} />
-      <TaskList tasks={tasks} deleteTask={deleteTask} />
+      <TaskForm/>
+      <TaskList/>
     </>
   );
 }
--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,12 +1,38 @@
-import { Children, createContext } from "react";
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/tasks";
 
 export const TaskContext = createContext();
 
 export function TaskContextProvider(props) {
-  let x = 20;
+  const [tasks, setTasks] = useState([]);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]); //Valore que contiene el arreglo, valores nuevos que tendrá el arreglo
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
 
   return (
-    <TaskContext.Provider value={x}>
+    <TaskContext.Provider
+      value={{
+        tasks,
+        deleteTask,
+        createTask,
+      }}
+    >
       {props.children}
     </TaskContext.Provider>
   );
